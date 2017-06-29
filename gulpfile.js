5c8e0b16209a18bcb4585a6479d57af9646aa281
Roberto Torres <roberto.torres.dev@gmail.com>
2017-06-29 12:52:41 -0700

    gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,18 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');//Runs a local dev server
 var open = require('gulp-open');//Open the url on the browser
+var browserify = require('browserify');
+var reactify = require('reactify');
+var source = require('vinyl-source-stream');
 
 var config = {
   port: 9085,
   devBaseUrl: 'http://localhost',
   paths: {
     html: './src/*.html',
-    dist: './dist'
+    js: '.src/**/*.js',
+    dist: './dist',
+    mainJs: './src/main.js'
   }
 }
 
@@ -31,8 +36,19 @@ gulp.task('html', function () {
   gulp.src(config.paths.html).pipe(gulp.dest(config.paths.dist)).pipe(connect.reload());
 });
 
+gulp.task('js', function (){
+  browserify(config.paths.mainJs)
+  .transform(reactify)
+  .bundle()
+  .on('error', console.error.bind(console))
+  .pipe(source('bundle.js'))
+  .pipe(gulp.dest(config.paths.dist + '/scripts'))
+  .pipe(connect.reload());
+})
+
 gulp.task('watch', function () {
-  gulp.watch(config.paths.html, ['html'])
+  gulp.watch(config.paths.html, ['html']);
+  gulp.watch(config.paths.js, ['js']);
 });
 
-gulp.task('default', ['html', 'open', 'watch']);
+gulp.task('default', ['html', 'js', 'open', 'watch']);
